d1d8442e6ee72dcc727acc4b74c91e532ac1f47a
Jim Yang <jyang_123@hotmail.com>
2018-12-10 20:13:48 -0500

    Added shipped and received functions

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,14 +1,16 @@
-// solhint-disable-next-line
 pragma solidity ^0.4.25;
 
 contract Escrow {
 	uint public value;
 	address public buyer;
 	address public seller;
+	enum State { Created, Confirmed, Shipped, Completed, Aborted }
+	State public state;
 
 	constructor() public payable {
 		seller = msg.sender;
 		value = msg.value / 2;
+		state = State.Created;
 	}
 
 	modifier onlyBuyer() {
@@ -21,5 +23,29 @@ contract Escrow {
 		_;
 	}
 
+	modifier inState(State requiredState) {
+		require (state == requiredState);
+		_;
+	}
 
+	function abort() public onlySeller inState(State.Created) {
+		state = State.Aborted;
+		seller.transfer(address(this).balance);
+	}
+
+	function confirmPurchase() public payable inState(State.Created) {
+		require (msg.value == value * 2);
+		buyer = msg.sender();
+		state = State.Confirmed;
+	}
+
+	function ship() public onlySeller inState(State.Confirmed) {
+		state = State.Shipped;
+	}
+
+	function confirmReceived() public onlyBuyer inState(State.Shipped) {
+		state = State.Completed;
+		buyer.transfer(value);
+		seller.transfer(address(this).balance);
+	}
 }
